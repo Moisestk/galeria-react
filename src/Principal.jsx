5d94b59ee3d9e34331fbda291dbe840ac232b06e
Moisestk <moisestodoroki@gmail.com>
2025-01-27 14:13:30 -0400

    Agregando cambios1

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,6 +1,6 @@
-import Navegacion from './Componentes/Nav';
-import Fotos from './Componentes/Part';
-import Vistas from './Componentes/Vistas';
+import Navegacion from '/Componentes/Nav';
+import Fotos from '/Componentes/Part';
+import Vistas from '/Componentes/Vistas';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function Principal() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Principal from './Principal.jsx'
+import Principal from '/Principal.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
