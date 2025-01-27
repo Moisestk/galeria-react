ec74c5a1921d75fbcc646f431a339f4dbb890e10
Moisestk <moisestodoroki@gmail.com>
2025-01-27 14:55:04 -0400

    Merge branch 'master' of https://github.com/Moisestk/galeria-react

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://moisestk.github.io/galeria-react/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+  
+}
+)
